2915425d828b5d140ced415e9df4f90c147142a7
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-12 03:03:38 +0000

    added toasts and react-hook-form

--- project/src/app/components/ui/LoginForm.tsx
+++ project/src/app/components/ui/LoginForm.tsx
@@ -9,38 +9,78 @@ import {
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { useForm } from 'react-hook-form';
+import Loading from "./loading-spinner";
 
+interface UserInput {
+  email: string;
+  password: string;
+ 
+}
 export function LoginForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
+  const {register,handleSubmit, watch,formState:{errors,isSubmitting},setError} = useForm<UserInput>()
+  const [loading,setLoading] = useState(false)
+  const router = useRouter()
+  
+
+  const onSubmit = async(data:UserInput) => {
+    setLoading(true)
+    // Handle form submission here (e.g., send data to server)
+    console.log(data);
+     try {
+      const response = await axios.post("/api/user/signin",data)
+      setLoading(false)
+      toast.success(response.data.message)
+      // setLoggedIn(true)
+      // await awaitDelay(2)
+      router.push("/")
+     } catch (error:any) {
+      setLoading(false)
+      toast.error(error.response.data.message || "An error from our side, please try again")
+     }
+     
+      // Reset form fields
+  }
   return (
     <div className="max-w-md w-full mx-auto my-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome Back to WeBlog
       </h2>
+      
      
 
-      <form className="my-8" onSubmit={handleSubmit}>
-       
+      <form className="my-8" onSubmit={handleSubmit(onSubmit)}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="example@somemail.com" type="email" />
+          <Input id="email" placeholder="example@somemail.com" type="email"
+           {...register("email" ,{required:{value:true, message:"Email is required"}})}
+            />
+          {errors.email? <div className='text-sm text-yellow-500 font-bold'>{errors.email.message}</div>:""}
+
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input id="password" placeholder="••••••••" type="password"
+          {...register("password" ,{required:{value:true, message:"Password is required"},minLength:{value:3,message:"Password must be atleast 3 characters long"}})}
+          />
+      {errors.password? <div className='text-sm text-yellow-500 font-bold'>{errors.password.message}</div>:""}
+
         </LabelInputContainer>
 
-        <button
+        {
+       loading?<div><Loading/></div>: <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-       Login &rarr;
+          Login &rarr;
           <BottomGradient />
         </button>
-
+}
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
       </form>    </div>
--- project/src/app/components/ui/SignUpForm.tsx
+++ project/src/app/components/ui/SignUpForm.tsx
@@ -9,32 +9,76 @@ import {
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
 import Link from "next/link";
-
+import { useForm } from 'react-hook-form';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import Loading from "./loading-spinner";
+interface UserInput {
+  email: string;
+  username: string;
+  password: string;
+  confirmpassword:string;
+}
 export function SignUpForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  const {register,handleSubmit, watch,formState:{errors,isSubmitting},setError} = useForm<UserInput>()
+  const [loading,setLoading] = useState(false)
+  const router = useRouter()
+  
+
+  const onSubmit = async(data:UserInput) => {
+   
+    // Handle form submission here (e.g., send data to server)
+    console.log(data);
+   try {
+    setLoading(true)
+    if(data.password != data.confirmpassword){
+      setError("confirmpassword",{type:"custom", message:"Password and Confirm Password fields not matching"})
+      setLoading(false)
+      return
+    }
+     const response = await axios.post("/api/user/signup",data)
+     setLoading(false)
+     toast.success(response.data.message) 
+     router.push("/user/signin")
+    } catch (error:any) {
+      setLoading(false)
+      toast.error(error.response.data.message || "An error occured please try again")
+   }
+ 
   };
   return (
     <div className="max-w-md w-full mx-auto my-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+          
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to WeBlog
       </h2>
      
 
-      <form className="my-8" onSubmit={handleSubmit}>
+      <form className="my-8" onSubmit={handleSubmit(onSubmit)}>
        
         <LabelInputContainer className="mb-4">
           <Label htmlFor="username">Username</Label>
-          <Input id="username" placeholder="This will be your public name" type="text" />
+          <Input id="username"  {...register("username" ,{required:{value:true, message:"Username is required"}, 
+            minLength:{value:4, message: `Username must be atleast 4 characters long`}})} placeholder="This will be your public name" type="text" />
+        {errors.username? <div className='text-sm text-yellow-500 font-bold'>{errors.username.message}</div>:""}
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="example@somemail.com" type="email" />
+          <Input id="email" placeholder="example@somemail.com" type="email"
+        {...register("email" ,{required:{value:true, message:"Email is required"}})}
+          />
+        {errors.email? <div className='text-sm text-yellow-500 font-bold'>{errors.email.message}</div>:""}
         </LabelInputContainer>
+
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input id="password" placeholder="••••••••" type="password" 
+                      {...register("password" ,{required:{value:true, message:"Password is required"},minLength:{value:3,message:"Password must be atleast 3 characters long"}})}
+
+          />
+        {errors.password? <div className='text-sm text-yellow-500 font-bold'>{errors.password.message}</div>:""}
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
           <Label htmlFor="confirmpassword">Confirm Password</Label>
@@ -42,17 +86,21 @@ export function SignUpForm() {
             id="confirmpassword"
             placeholder="••••••••"
             type="password"
+            {...register("confirmpassword" ,{required:{value:true, message:"Confirm Password is required"},minLength:{value:3,message:"Password must be atleast 3 characters long"}})}
+
           />
+        {errors.confirmpassword && <div className='text-sm text-yellow-500 font-bold'>{errors.confirmpassword.message}</div>}
         </LabelInputContainer>
 
-        <button
+{
+       loading?<div><Loading/></div>: <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
           Sign up &rarr;
           <BottomGradient />
         </button>
-
+}
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
       </form>
