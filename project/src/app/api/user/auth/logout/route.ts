62950bad7094963d6e12b6c7efd48b6b54aa68ab
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-13 23:36:57 +0530

    add logout API route

---
+++ project/src/app/api/user/auth/logout/route.ts
@@ -0,0 +1,22 @@
+import connectDB from "@/dbConfig/dbConfig";
+import User from "@/models/user-schema";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { cookies } from "next/headers";
+connectDB()
+export async function GET(){
+   try {
+      const token = cookies()?.get("token")?.value
+      if(!token){
+          return NextResponse.json({message:"Unauthorized user"},{status:401})
+      }
+      const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as jwt.JwtPayload
+      const userId = decodedToken.id
+      const user = await User.findOne({_id:userId}).select("username")
+   cookies().delete("token")
+   return NextResponse.json({message:`${user.username} signed out successfully`},{status:200})
+} catch (error:any) {
+    return NextResponse.json({message:error.message},{status:500})
+   }
+}
